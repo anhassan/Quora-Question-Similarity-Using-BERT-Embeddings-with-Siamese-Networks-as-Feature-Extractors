652e905834680d804924968f9878b7ff462996e4
anhassan <51217263+anhassan@users.noreply.github.com>
2019-09-29 21:52:55 -0700

    BERT based sentence features

---
+++ Feature_Engineering_BERT.py
@@ -0,0 +1,112 @@
+"""
+Detecting duplicate quora questions
+feature engineering
+@author: Abhishek Thakur
+"""
+import nltk
+nltk.download('stopwords')
+import _pickle as cPickle
+import pandas as pd
+import numpy as np
+import gensim
+from fuzzywuzzy import fuzz
+from nltk.corpus import stopwords
+from tqdm import tqdm
+from scipy.stats import skew, kurtosis
+from scipy.spatial.distance import cosine, cityblock, jaccard, canberra, euclidean, minkowski, braycurtis
+
+from nltk import word_tokenize
+stop_words = stopwords.words('english')
+
+from numpy import genfromtxt
+
+def wmd(s1, s2):
+    s1 = str(s1).lower().split()
+    s2 = str(s2).lower().split()
+    stop_words = stopwords.words('english')
+    s1 = [w for w in s1 if w not in stop_words]
+    s2 = [w for w in s2 if w not in stop_words]
+    return model.wmdistance(s1, s2)
+
+
+def norm_wmd(s1, s2):
+    s1 = str(s1).lower().split()
+    s2 = str(s2).lower().split()
+    stop_words = stopwords.words('english')
+    s1 = [w for w in s1 if w not in stop_words]
+    s2 = [w for w in s2 if w not in stop_words]
+    return norm_model.wmdistance(s1, s2)
+
+
+def sent2vec(s):
+    words = str(s).lower().decode('utf-8')
+    words = word_tokenize(words)
+    words = [w for w in words if not w in stop_words]
+    words = [w for w in words if w.isalpha()]
+    M = []
+    for w in words:
+        try:
+            M.append(model[w])
+        except:
+            continue
+    M = np.array(M)
+    v = M.sum(axis=0)
+    return v / np.sqrt((v ** 2).sum())
+
+
+data = pd.read_csv('./data_balanced.csv')
+data = data.drop(['id', 'qid1', 'qid2'], axis=1)
+
+print('Done Reading Data')
+data['len_q1'] = data.question1.apply(lambda x: len(str(x)))
+data['len_q2'] = data.question2.apply(lambda x: len(str(x)))
+data['diff_len'] = data.len_q1 - data.len_q2
+data['len_char_q1'] = data.question1.apply(lambda x: len(''.join(set(str(x).replace(' ', '')))))
+data['len_char_q2'] = data.question2.apply(lambda x: len(''.join(set(str(x).replace(' ', '')))))
+data['len_word_q1'] = data.question1.apply(lambda x: len(str(x).split()))
+data['len_word_q2'] = data.question2.apply(lambda x: len(str(x).split()))
+data['common_words'] = data.apply(lambda x: len(set(str(x['question1']).lower().split()).intersection(set(str(x['question2']).lower().split()))), axis=1)
+data['fuzz_qratio'] = data.apply(lambda x: fuzz.QRatio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_WRatio'] = data.apply(lambda x: fuzz.WRatio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_partial_ratio'] = data.apply(lambda x: fuzz.partial_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_partial_token_set_ratio'] = data.apply(lambda x: fuzz.partial_token_set_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_partial_token_sort_ratio'] = data.apply(lambda x: fuzz.partial_token_sort_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_token_set_ratio'] = data.apply(lambda x: fuzz.token_set_ratio(str(x['question1']), str(x['question2'])), axis=1)
+data['fuzz_token_sort_ratio'] = data.apply(lambda x: fuzz.token_sort_ratio(str(x['question1']), str(x['question2'])), axis=1)
+
+print('Done Fuzzy..Starting Vectors')
+bert_q = genfromtxt('/home/ganesh/Quora_dev/tmp/Ganesh_MSCI/Unbalanced_Embeddings/bert/bert_q1_balanced.csv', delimiter=',',skip_header=1)
+question1_vectors = np.delete(bert_q,0,1)
+bert_q = genfromtxt('/home/ganesh/Quora_dev/tmp/Ganesh_MSCI/Unbalanced_Embeddings/bert/bert_q2_balanced.csv', delimiter=',',skip_header=1)
+question2_vectors = np.delete(bert_q,0,1)
+ 
+data['cosine_distance'] = [cosine(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['cityblock_distance'] = [cityblock(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['jaccard_distance'] = [jaccard(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['canberra_distance'] = [canberra(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['euclidean_distance'] = [euclidean(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['minkowski_distance'] = [minkowski(x, y, 3) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['braycurtis_distance'] = [braycurtis(x, y) for (x, y) in zip(np.nan_to_num(question1_vectors),
+                                                          np.nan_to_num(question2_vectors))]
+
+data['skew_q1vec'] = [skew(x) for x in np.nan_to_num(question1_vectors)]
+data['skew_q2vec'] = [skew(x) for x in np.nan_to_num(question2_vectors)]
+data['kur_q1vec'] = [kurtosis(x) for x in np.nan_to_num(question1_vectors)]
+data['kur_q2vec'] = [kurtosis(x) for x in np.nan_to_num(question2_vectors)]
+print('Downloading...')
+# cPickle.dump(question1_vectors, open('data/q1_w2v.pkl', 'wb'), -1)
+# cPickle.dump(question2_vectors, open('data/q2_w2v.pkl', 'wb'), -1)
+
+data.to_csv('data/features_From_BERT_Vectors.csv', index=False)
